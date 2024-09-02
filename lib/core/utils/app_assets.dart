9539c85eeb00de9b851fa64e00679572a7750ab7
Solama661 <Solama661@gmail.com>
2024-09-02 22:59:25 +0300

    app_assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,89 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImagesAuthLoginSuccess
+  /// assets/images/auth/Login-success.png
+  static const String assetsImagesAuthLoginSuccess = "assets/images/auth/Login-success.png";
+
+  /// Assets for assetsImagesAuthSignUpSuccess
+  /// assets/images/auth/signUp-success.png
+  static const String assetsImagesAuthSignUpSuccess = "assets/images/auth/signUp-success.png";
+
+  /// Assets for assetsImagesBannersImage 1
+  /// assets/images/banners/image 1.png
+  static const String assetsImagesBannersImage1 = "assets/images/banners/image 1.png";
+
+  /// Assets for assetsImagesBannersYoungBeautifulFashionableCaucasianWomanHoldsShoppingBagsOneHandSmartphoneAnotherIsolatedWhite 1
+  /// assets/images/banners/young-beautiful-fashionable-caucasian-woman-holds-shopping-bags-one-hand-smartphone-another-isolated-white 1.png
+  static const String  banner1 = "assets/images/banners/young-beautiful-fashionable-caucasian-woman-holds-shopping-bags-one-hand-smartphone-another-isolated-white 1.png";
+
+  /// Assets for assetsImagesBottomNavBarSaved icon
+  /// assets/images/bottom_nav_bar/saved icon.png
+  static const String assetsImagesBottomNavBarSavedIcon = "assets/images/bottom_nav_bar/saved icon.png";
+
+  /// Assets for assetsImagesBottomNavBarShoppingCart
+  /// assets/images/bottom_nav_bar/shopping-cart.png
+  static const String assetsImagesBottomNavBarShoppingCart = "assets/images/bottom_nav_bar/shopping-cart.png";
+
+  /// Assets for assetsImagesBottomNavBarUser icon
+  /// assets/images/bottom_nav_bar/user icon.png
+  static const String assetsImagesBottomNavBarUserIcon = "assets/images/bottom_nav_bar/user icon.png";
+
+  /// Assets for assetsImagesBottomNavBarVector
+  /// assets/images/bottom_nav_bar/Vector.png
+  static const String assetsImagesBottomNavBarVector = "assets/images/bottom_nav_bar/Vector.png";
+
+  /// Assets for assetsImagesCategoriesGroup 2
+  /// assets/images/categories/Group 2.png
+  static const String assetsImagesCategoriesGroup2 = "assets/images/categories/Group 2.png";
+
+  /// Assets for assetsImagesCategoriesGroup 3
+  /// assets/images/categories/Group 3.png
+  static const String assetsImagesCategoriesGroup3 = "assets/images/categories/Group 3.png";
+
+  /// Assets for assetsImagesCategoriesGroup 4
+  /// assets/images/categories/Group 4.png
+  static const String assetsImagesCategoriesGroup4 = "assets/images/categories/Group 4.png";
+
+  /// Assets for assetsImagesCategoriesGroup 5
+  /// assets/images/categories/Group 5.png
+  static const String assetsImagesCategoriesGroup5 = "assets/images/categories/Group 5.png";
+
+  /// Assets for assetsImagesCategoriesGroup 6
+  /// assets/images/categories/Group 6.png
+  static const String assetsImagesCategoriesGroup6 = "assets/images/categories/Group 6.png";
+
+  /// Assets for assetsImagesLocationGroup 25
+  /// assets/images/location/Group 25.png
+  static const String assetsImagesLocationGroup25 = "assets/images/location/Group 25.png";
+
+  /// Assets for assetsImagesLogoSplash
+  /// assets/images/logo_splash.png
+  static const String assetsImagesLogoSplash = "assets/images/logo_splash.png";
+
+  /// Assets for assetsImagesProductsProduct1
+  /// assets/images/products/product-1.png
+  static const String assetsImagesProductsProduct1 = "assets/images/products/product-1.png";
+
+  /// Assets for assetsImagesProductsProduct2
+  /// assets/images/products/product-2.png
+  static const String assetsImagesProductsProduct2 = "assets/images/products/product-2.png";
+
+  /// Assets for assetsImagesProductsProduct3
+  /// assets/images/products/product-3.png
+  static const String assetsImagesProductsProduct3 = "assets/images/products/product-3.png";
+
+  /// Assets for assetsImagesProductsProduct4
+  /// assets/images/products/product-4.png
+  static const String assetsImagesProductsProduct4 = "assets/images/products/product-4.png";
+
+  /// Assets for assetsImagesProductsProduct5
+  /// assets/images/products/product-5.png
+  static const String assetsImagesProductsProduct5 = "assets/images/products/product-5.png";
+
+  /// Assets for assetsImagesProductsProsuct6
+  /// assets/images/products/prosuct-6.png
+  static const String assetsImagesProductsProsuct6 = "assets/images/products/prosuct-6.png";
+}
+
